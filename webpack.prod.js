4b9a37c6878b72e07cd041285e9b02fc0571191e
Vincent Chan <okcdz@diverse.space>
2018-05-20 17:13:05 +0800

    feature: bundle hash

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,13 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.config.js');
+const path = require('path');
 
 module.exports = merge(common, {
+  output: {
+    filename: 'bundle.[hash:8].js',
+    path: path.resolve(__dirname, 'dist')
+  },
   plugins: [
     new UglifyJSPlugin({
       sourceMap: true,
